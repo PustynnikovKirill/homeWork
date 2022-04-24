0e6c5005b0136e38534d8e425b1e3aa51687f898
Kirill Pustynnikov <kiryuha911@yandex.by>
2022-04-24 17:08:37 +0300

    'v'

--- homework/src/p2-homeworks/h1/Message.tsx
+++ homework/src/p2-homeworks/h1/Message.tsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import style from './Message.module.css'
 
-function Message() {
+export  type propsMessageType = {
+    avatar: string,
+    name: string,
+    message: string,
+    time: string,
+}
+
+function Message(props: propsMessageType) {
     return (
-        <div>
-
+        <div className={style.message}>
+            <img src={props.avatar} alt={'avatar'} className={style.avatar}/>
+            <div className={style.angle}/>
+                <div className={style.angle}>
+                    {props.name}
+                    {props.message}
+                    {props.time}
+                </div>
         </div>
     )
 }
